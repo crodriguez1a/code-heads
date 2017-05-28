2d0a81bb0ff753b533f22cb7f700e12878591108
Carlos Rodriguez <crodriguez1a@gmail.com>
2017-05-28 19:14:33 -0400

    Annotation

--- glimmer-codeheads/website/src/ui/components/article-card/component.ts
+++ glimmer-codeheads/website/src/ui/components/article-card/component.ts
@@ -1,8 +1,18 @@
 import Component, { tracked } from '@glimmer/component';
 
 export default class ArticleCard extends Component {
-  @tracked preview = true;
+  /**
+    Signal if preview content should be displayed
 
+    @property preview
+  */
+  @tracked preview: Boolean = true;
+
+  /**
+    Toggle the article's preview content
+
+    @method togglePreview
+  */
   private togglePreview(e) {
     e.preventDefault();
     this.preview = !this.preview;

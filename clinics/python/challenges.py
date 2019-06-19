046904b33953f014ce0e41dfcb7c174d7572a59b
Carlos Rodriguez <crodriguez1a@gmail.com>
2019-06-19 19:36:19 -0400

    Additional annoations

--- clinics/python/challenges.py
+++ clinics/python/challenges.py
@@ -1,8 +1,14 @@
+# answer key
+
 def big_vowels(text:str="hello"):
     result:str = ""
-    for i in text.lower():
+    for i in text.lower(): # normalize the input
+        # a member test on a set has O(1) complexity
+        # ternary is concise but still readable
         result += i.upper() if i in {'a','e','i','o','u'} else i
     
     return result
 
 print('answer:', big_vowels("what in the world")) # -> whAt In thE wOrld
+
+# TODO: add tests
